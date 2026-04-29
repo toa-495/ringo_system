95a1f9bd56b57e68abfe656cb6f38ebd6ac46ed1
toa-495 <k.oda@yumephoto.com>
2026-04-29 15:43:34 +0900

    Set GAS Web App URL in frontend config

---
+++ config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  GAS_BASE_URL: 'https://script.google.com/macros/s/AKfycbwc7ikPfb6u8h_d_is-xTTvfyWS5_T7VyUSJyesuVlgnCcY6UlfVaawAqR_mBDqqJzJ/exec',
+  DEFAULT_DUE_DAYS: 7,
+};
