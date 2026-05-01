076d3a17f4c80511b6fc3b883452728d776904c6
toa-495 <k.oda@yumephoto.com>
2026-05-01 15:01:23 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,3 +2,5 @@ window.APP_CONFIG = {
   GAS_BASE_URL: 'https://script.google.com/macros/s/AKfycbwc7ikPfb6u8h_d_is-xTTvfyWS5_T7VyUSJyesuVlgnCcY6UlfVaawAqR_mBDqqJzJ/exec',
   DEFAULT_DUE_DAYS: 7,
 };
+
+const RECEIPT_UPLOAD_URL = 'https://script.google.com/macros/s/AKfycbwrwbvOB3CCkIUBxOwViSCSWh6nYr9rhg8_8L3V7ROsb_f2hhvUmZhRydV5WWwqEkozFQ/exec';
